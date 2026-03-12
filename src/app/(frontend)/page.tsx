968e12d375a0d5fc18254acc97b29240d59bcb16
celal gökçe <celal_2003@outlook.com>
2026-03-12 20:03:24 +0300

    feat: update database URI in .env.example and adjust ESLint rule in HomePage component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import { headers as getHeaders } from 'next/headers.js'
 import { getPayload } from 'payload'
 import React from 'react'
-
 import config from '@payload-config'
 
 export default async function HomePage() {
